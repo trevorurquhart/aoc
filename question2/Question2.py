ed3492cc8c62558b5c80efaef84c0e17a4057edf
trevorurquhart <trevor.urquhart@gmail.com>
2022-12-04 12:47:14 +1300

    change question 2

--- question2/Question2.py
+++ question2/Question2.py
@@ -1,69 +1,67 @@
-ROCK = "A"
-PAPER = "B"
-SCISSORS = "C"
-
-LOSS = "X"
-DRAW = "Y"
-WIN = "Z"
-
-WIN_MAP = {ROCK: PAPER, PAPER: SCISSORS, SCISSORS: ROCK}
-LOSE_MAP = {ROCK: SCISSORS, PAPER: ROCK, SCISSORS: PAPER}
+class TheirMove:
+    ROCK = "A"
+    PAPER = "B"
+    SCISSORS = "C"
 
 
-def shape_to_win(opponent_move):
-    return WIN_MAP[opponent_move]
+class MyMove:
+    ROCK = "X"
+    PAPER = "Y"
+    SCISSORS = "Z"
 
 
-def shape_to_lose(opponent_move):
-    return LOSE_MAP[opponent_move]
+class Outcome:
+    LOSS = "X"
+    DRAW = "Y"
+    WIN = "Z"
+
+
+shape_to_win = {TheirMove.ROCK: MyMove.PAPER, TheirMove.PAPER: MyMove.SCISSORS, TheirMove.SCISSORS: MyMove.ROCK}
+to_lose = {TheirMove.ROCK: MyMove.SCISSORS, TheirMove.PAPER: MyMove.ROCK, TheirMove.SCISSORS: MyMove.PAPER}
+shape_to_draw = {TheirMove.ROCK: MyMove.ROCK, TheirMove.PAPER: MyMove.PAPER, TheirMove.SCISSORS: MyMove.SCISSORS}
 
 
 def shape_score(shape):
-    return 1 if shape == ROCK else (2 if shape == PAPER else 3)
+    return 1 if shape == MyMove.ROCK else (2 if shape == MyMove.PAPER else 3)
 
 
 def game_score(game_outcome):
-    return 6 if game_outcome == WIN else (3 if game_outcome == DRAW else 0)
+    return 6 if game_outcome == Outcome.WIN else (3 if game_outcome == Outcome.DRAW else 0)
 
 
 def game_result(their_shape, my_shape):
-    if my_shape == shape_to_win(their_shape):
-        return WIN
+    if my_shape == shape_to_win[their_shape]:
+        return Outcome.WIN
 
-    if my_shape == their_shape:
-        return DRAW
+    if my_shape == shape_to_draw[their_shape]:
+        return Outcome.DRAW
 
-    return LOSS
+    return Outcome.LOSS
 
 
 def shape_to_play(their_shape, result):
-    if result == WIN:
-        return shape_to_win(their_shape)
-    elif result == LOSS:
-        return shape_to_lose(their_shape)
+    if result == Outcome.WIN:
+        return shape_to_win[their_shape]
+    elif result == Outcome.LOSS:
+        return to_lose[their_shape]
     else:
-        return their_shape
-
-
-def shape(code):
-    return ROCK if code in ("A", "X") else (PAPER if code in ("B", "Y") else SCISSORS)
+        return shape_to_draw[their_shape]
 
 
 def solve_one(lines):
     score = 0
     for line in lines:
-        their_shape, my_shape = [shape(s) for s in line.split(' ')]
+        their_shape, my_shape = [s for s in line.split(' ')]
         score += shape_score(my_shape) + game_score(game_result(their_shape, my_shape))
     print(score)
 
 
 def solve_two(lines):
-    total = 0
+    score = 0
     for line in lines:
         their_shape_code, result_code = line.split(' ')
-        total += shape_score(shape_to_play(their_shape_code, result_code)) + game_score(result_code)
-
-    print(total)
+        score += shape_score(shape_to_play(their_shape_code, result_code)) + game_score(result_code)
+    print(score)
 
 
 with open('question_data.txt') as f:
