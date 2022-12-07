3200703dfdc9d1a38a8f73dc573223099b3f4fc6
trevorurquhart <trevor.urquhart@gmail.com>
2022-12-08 06:51:36 +1300

    Question 7

--- question7/Question7.py
+++ question7/Question7.py
@@ -5,88 +5,94 @@ class Directory:
         self.files = []
         self.directories = []
         self.parent = parent
+        separator = "" if parent is None or parent.is_root() else "/"
+        self.full_path = name if parent is None else parent.full_path + separator + name
 
     def cd(self, arg):
-        if arg == "..":
-            return self.parent
-        if arg == "/":
-            d = self
-            while d.parent is not None:
-                d = d.parent
-            return d
+        match arg:
+            case "..":
+                return self.parent
 
-        # cd arg
-        dir = next((d for d in self.directories if d.name == arg), None)
-        if dir is None:
-            self.add_dir(arg)
-        return dir
+            case "/":
+                d = self
+                while d.parent is not None:
+                    d = d.parent
+                return d
 
-    def add_dir(self, dir_name):
-        self.directories.append(Directory(dir_name, self))
+            case _:
+                d = next((d for d in self.directories if d.name == arg), None)
+                if d is None:
+                    d = self.mkdir(arg)
+        return d
 
-    def add_file(self, name, size):
+    def mkdir(self, dir_name):
+        d = Directory(dir_name, self)
+        self.directories.append(d)
+        return d
+
+    def mkfile(self, name, size):
         self.files.append((name, size))
 
     def file_size(self):
         files = list(zip(*self.files))
         return sum(files[1]) if len(files) > 0 else 0
 
-    def visit(self, visitor, parents=[]):
-        dir_size = self.file_size()
-        full_path = "/" if len(parents) == 0 else parents[-1:][0] + self.name + "/"
-        visitor[full_path] = dir_size
-        for p in parents:
-            visitor[p] += dir_size
+    def is_root(self):
+        return self.parent is None
 
-        p = parents + [full_path]
+    def dir_sizes(self, ans=None, parents=None):
+        if parents is None:
+            parents = []
+        if ans is None:
+            ans = {}
+        dir_size = self.file_size()
+        ans[self.full_path] = dir_size
+        for p in parents:
+            ans[p] += dir_size
+
         for d in self.directories:
-            d.visit(visitor, p)
+            d.dir_sizes(ans, parents + [self.full_path])
+
+        return ans
 
     def tree(self, indent=""):
         for f in self.files:
             print(indent + "- " + f[0] + " (" + str(f[1]) + ")")
         for d in self.directories:
-            print(indent + "- " + d.name + " (dir)")
+            print(indent + "+ " + d.name + " (dir)")
             d.tree(indent + "  ")
 
     def solve_one(self):
-        dir_sizes = {}
-        self.visit(dir_sizes)
-        print(sum([size for d, size in dir_sizes.items() if size <= 100000]))
+        print(sum([size for d, size in self.dir_sizes().items() if size <= 100000]))
 
     def solve_two(self):
-        dir_sizes = {}
-        self.visit(dir_sizes)
-        used_space = dir_sizes['/']
-        free_now = 70000000 - used_space
-        to_delete = 30000000 - free_now
+        dir_sizes = self.dir_sizes()
+        to_delete = 30000000 - (70000000 - dir_sizes['/'])
         print(min([size for d, size in dir_sizes.items() if size >= to_delete]))
 
 
-def parse_dir(lines):
-    dir = None
-    for line in lines:
-        cmd = line.split(" ")
-        a = cmd[0]
-        b = cmd[1] if len(cmd) > 1 else None
-        if a == "cd":
-            if dir is None:
-                dir = Directory(b)
-            else:
-                dir = dir.cd(b)
+def parse_dir(cmds):
+    d = None
+    for cmd in cmds:
+        match cmd.split():
+            case ["cd", arg]:
+                if d is None:
+                    d = Directory(arg)
+                else:
+                    d = d.cd(arg)
 
-        if a == "dir":
-            dir.add_dir(b)
+            case ["dir", arg]:
+                d.mkdir(arg)
 
-        if a.isdigit():
-            dir.add_file(b, int(a))
+            case [size, file] if size.isdigit():
+                d.mkfile(file, int(size))
 
-    return dir.cd("/")
+    return d.cd("/")
 
 
-with open('question_data.txt') as f:
-    lines = [line.replace('$', '').strip() for line in f.readlines()]
-    dir = parse_dir(lines)
-    # dir.tree()
-    dir.solve_one()
-    dir.solve_two()
+with open('question_data.txt') as q:
+    lines = [line.replace('$', '').strip() for line in q.readlines()]
+    directory = parse_dir(lines)
+    # directory.tree()
+    directory.solve_one()
+    directory.solve_two()
