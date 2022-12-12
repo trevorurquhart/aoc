979d70f70e821e020090ab31c3f07eb273e5598c
trevorurquhart <trevor.urquhart@gmail.com>
2022-12-12 16:34:54 +1300

    question 11

--- question11/Question11.py
+++ question11/Question11.py
@@ -3,19 +3,19 @@ import math
 
 class Monkey:
 
-    def __init__(self, items, op, test, if_true, if_false):
+    def __init__(self, items, operation, is_divisible_by, monkey1, monkey2):
         self.items = items
-        self.op = op
-        self.test = test
-        self.if_true = if_true
-        self.if_false = if_false
+        self.op = operation
+        self.is_divisible_by = is_divisible_by
+        self.monkey1 = monkey1
+        self.monkey2 = monkey2
         self.no_of_inspections = 0
 
     def add_item(self, item):
         self.items.append(item)
 
     def throw(self, item, monkeys):
-        target_monkey = self.if_true if item % self.test == 0 else self.if_false
+        target_monkey = self.monkey1 if item % self.is_divisible_by == 0 else self.monkey2
         monkeys[target_monkey].add_item(item)
 
     def inspect(self, monkeys, worry_fn):
@@ -25,25 +25,25 @@ class Monkey:
             self.throw(item, monkeys)
 
 
-def parse_monkeys(lines):
+def parse_monkeys(data):
     monkeys = []
-    params = []
-    for line in lines:
-        match line.split():
-            case ["Starting", "items:", *items]:
-                params.append([int(item.replace(",", "")) for item in items])
-            case ["Operation:", "new", "=", "old", "*", m]:
-                params.append((lambda x: x * x) if m == "old" else (lambda x, m=m: x * int(m)))
-            case ["Operation:", "new", "=", "old", "+", a]:
-                params.append((lambda x: x + x) if a == "old" else (lambda x, a=a: x + int(a)))
-            case ["Test:", "divisible", "by", s]:
-                params.append(int(s))
-            case ["If", "true:", "throw", "to", "monkey", true_monkey]:
-                params.append(int(true_monkey))
-            case ["If", "false:", "throw", "to", "monkey", false_monkey]:
-                params.append(int(false_monkey))
-                monkeys.append(Monkey(*params))
-                params = []
+    for group in data.split("\n\n"):
+        params = []
+        for line in [l.strip() for l in group.split("\n")]:
+            match line.split():
+                case ["Starting", "items:", *items]:
+                    params.append([int(item.replace(",", "")) for item in items])
+                case ["Operation:", "new", "=", "old", "*", m]:
+                    params.append((lambda x: x * x) if m == "old" else (lambda x, m=m: x * int(m)))
+                case ["Operation:", "new", "=", "old", "+", a]:
+                    params.append((lambda x: x + x) if a == "old" else (lambda x, a=a: x + int(a)))
+                case ["Test:", "divisible", "by", s]:
+                    params.append(int(s))
+                case ["If", "true:", "throw", "to", "monkey", true_monkey]:
+                    params.append(int(true_monkey))
+                case ["If", "false:", "throw", "to", "monkey", false_monkey]:
+                    params.append(int(false_monkey))
+        monkeys.append(Monkey(*params))
     return monkeys
 
 
@@ -57,17 +57,16 @@ def solve(monkeys, rounds, worry_fn):
     print(top * second)
 
 
-def solve_one(lines):
-    solve(parse_monkeys(lines), 20, lambda x: x // 3)
+def solve_one(monkeys):
+    solve(monkeys, 20, lambda x: x // 3)
 
 
-def solve_two(lines):
-    monkeys = parse_monkeys(lines)
-    lcm = math.lcm(*[m.test for m in monkeys])
+def solve_two(monkeys):
+    lcm = math.lcm(*[m.is_divisible_by for m in monkeys])
     solve(monkeys, 10000, lambda x: x % lcm)
 
 
 with open('question_data.txt') as q:
-    lines = [line.strip() for line in q.readlines()]
-    solve_one(lines)
-    solve_two(lines)
+    data = q.read()
+    solve_one(parse_monkeys(data))
+    solve_two(parse_monkeys(data))
