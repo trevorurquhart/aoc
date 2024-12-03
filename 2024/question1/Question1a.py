2f4d75abbb54883d21dd3d9c7cccdf4eab4d3805
trevorurquhart <trevor.urquhart@gmail.com>
2024-12-03 13:48:31 +1300

    q1 tidy

--- 2024/question1/Question1a.py
+++
@@ -1,6 +0,0 @@
-with open('input.txt') as f:
-    lines = f.readlines()
-    firstList = sorted([int(s.strip().split('   ')[0]) for s in lines])
-    secondList = sorted([int(s.strip().split('   ')[1]) for s in lines])
-    x = sum([abs(firstList[n] - secondList[n]) for n  in range(len(firstList))])
-    print(x)
--- 2024/question1/Question1b.py
+++ 2024/question1/Question1b.py
@@ -1,6 +1,18 @@
+def solve_two(list_one, list_two):
+    multiplier = {x: list_two.count(x) for x in list(set(list_two))}
+    print(sum([x * multiplier.get(x, 0) for x in list_one]))
+
+
+def solve_one(list_one, list_two):
+    sorted_list_one = sorted(list_one)
+    sorted_list_two = sorted(list_two)
+    x = sum([abs(sorted_list_one[n] - sorted_list_two[n]) for n in range(len(list_one))])
+    print(x)
+
+
 with open('input.txt') as f:
     lines = f.readlines()
-    firstList = [int(s.strip().split('   ')[0]) for s in lines]
-    secondList = [int(s.strip().split('   ')[1]) for s in lines]
-    multiplier = {x: secondList.count(x) for x in list(set(secondList))}
-    print(sum([x * multiplier.get(x, 0) for x in firstList]))
+    first_list = [int(s.strip().split('   ')[0]) for s in lines]
+    second_list = [int(s.strip().split('   ')[1]) for s in lines]
+    solve_one(first_list, second_list)
+    solve_two(first_list, second_list)
