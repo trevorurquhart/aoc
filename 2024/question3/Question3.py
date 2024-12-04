b4e6a601fac22fbe30d0ceeeaddfe99a4b39ca0b
trevorurquhart <trevor.urquhart@gmail.com>
2024-12-04 14:44:04 +1300

    q3

---
+++ 2024/question3/Question3.py
@@ -0,0 +1,15 @@
+import re
+
+def solve_two(input):
+    delimited = 'do()' + input.replace('\n', '') + 'don\'t()'
+    split = re.findall('do\(\).*?don\'t\(\)', delimited)
+    return sum([solve_one(x) for x in split])
+
+def solve_one(input):
+    search = re.findall('mul\\(([0-9]{1,3}),([0-9]{1,3})\)', input)
+    return sum([int(x) * int(y) for x, y in search])
+
+
+with open('input.txt') as f:
+    print(solve_one(f.read()))
+    print(solve_two(f.read()))
