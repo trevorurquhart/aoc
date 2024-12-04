ff06b4e0cea95bc0cf67933683e6b371d4a66c2e
trevorurquhart <trevor.urquhart@gmail.com>
2024-12-05 08:52:02 +1300

    q4 tidy

--- 2024/question4/Question4.py
+++ 2024/question4/Question4.py
@@ -1,26 +1,26 @@
 offsets = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
 
-def word(grid, x, y, d_x, d_y, len):
-    return ''.join([grid[y + n * d_y][x + n * d_x] for n in range(len)])
+def word(grid, x, y, dx, dy, len):
+    return ''.join([grid[y + n * dy][x + n * dx] for n in range(len)])
 
 
 def search_for_xmas(grid, x, y):
-    return sum([check_for_xmas(grid, x, y, d_x, d_y) for d_x, d_y in offsets])
+    return sum([check_for_xmas(grid, x, y, dx, dy) for dx, dy in offsets])
 
 
-def check_for_xmas(grid, x, y, d_x, d_y):
-    w_len = 4
-    x_inbounds = d_x == 0 or (d_x == 1 and x <= len(grid[0]) - w_len) or (d_x == -1 and x >= 3)
-    y_inbounds = d_y == 0 or (d_y == 1 and y <= len(grid) - w_len) or (d_y == -1 and y >= 3)
-    return 1 if x_inbounds and y_inbounds and 'XMAS' == word(grid, x, y, d_x, d_y, w_len) else 0
+def check_for_xmas(grid, x, y, dx, dy):
+    word_len = len('XMAS')
+    x_ok = dx == 0 or (dx == 1 and x <= len(grid[0]) - word_len) or (dx == -1 and x >= 3)
+    y_ok = dy == 0 or (dy == 1 and y <= len(grid) - word_len) or (dy == -1 and y >= 3)
+    return 1 if x_ok and y_ok and 'XMAS' == word(grid, x, y, dx, dy, word_len) else 0
 
 
 def search_for_x_mas(grid, x, y):
-    x_inbounds = x <= len(grid[0]) - 3
-    y_inbounds = y <= len(grid) - 3
-    return 1 if x_inbounds \
-                and y_inbounds \
+    x_ok = x <= len(grid[0]) - 3
+    y_ok = y <= len(grid) - 3
+    return 1 if x_ok \
+                and y_ok \
                 and word(grid, x, y, 1, 1, 3) in {'MAS', 'SAM'} \
                 and word(grid, x + 2, y, -1, 1, 3) in {'MAS', 'SAM'} \
         else 0
