5337160178382a064d460d1d682a8aa30ca2f2b8
trevorurquhart <trevor.urquhart@gmail.com>
2024-12-06 10:56:35 +1300

    switch to defaultdict

--- 2024/question5/Question5.py
+++ 2024/question5/Question5.py
@@ -1,45 +1,33 @@
-class Page:
-
-    def __init__(self, value):
-        self.value = value
-        self.pages_before = set()
-
-    def before(self, page):
-        self.pages_before.add(page.value)
-
-    def is_before(self, v):
-        return v in self.pages_before
+from collections import defaultdict
 
 
-def sort(page_numbers, pages):
+def sort(pg_nums, pages):
     result = []
-    for p in page_numbers:
-        ins = next((idx for idx, v in enumerate(result) if pages.get(p).is_before(v)), len(result))
+    for p in pg_nums:
+        ins = next((idx for idx, v in enumerate(result) if v in pages.get(p)), len(result))
         result.insert(ins, p)
     return result
 
 
 def parse(rules):
-    pages = {}
+    pages = defaultdict(list)
     for before, after in rules:
-        before_page = pages.setdefault(before, Page(before))
-        after_page = pages.setdefault(after, Page(after))
-        before_page.before(after_page)
+        pages[before].append(after)
     return pages
 
 
-def count_valid(page_numbers, pages):
-    for idx, p in enumerate(page_numbers):
-        if any(pages.get(p).is_before(e) for e in page_numbers[:idx]):
+def count_valid(pg_nums, pages):
+    for idx, p in enumerate(pg_nums):
+        if any(e in pages.get(p) for e in pg_nums[:idx]):
             return 0
-    return page_numbers[len(page_numbers)//2]
+    return pg_nums[len(pg_nums) // 2]
 
 
-def count_in_valid(page_numbers, pages):
-    for idx, p in enumerate(page_numbers):
-        if any(pages.get(p).is_before(e) for e in page_numbers[:idx]):
-            fix_order = sort(page_numbers, pages)
-            return fix_order[len(fix_order) // 2]
+def count_invalid(pg_nums, pages):
+    for idx, p in enumerate(pg_nums):
+        if any(e in pages.get(p) for e in pg_nums[:idx]):
+            ordered = sort(pg_nums, pages)
+            return ordered[len(ordered) // 2]
     return 0
 
 
@@ -48,14 +36,12 @@ def solve_one(pages, page_numbers):
 
 
 def solve_two(pages, page_numbers):
-    print(sum([count_in_valid(pn, pages) for pn in page_numbers]))
+    print(sum([count_invalid(pn, pages) for pn in page_numbers]))
 
 
 with open('input_order.txt') as o:
     with open('input_updates.txt') as u:
-        rules = [r.strip() for r in o.readlines()]
-        updates = [u.strip() for u in u.readlines()]
-        pages = parse([(int(x), int(y)) for x, y in [r.split('|') for r in rules]])
-        page_numbers = [[int(p) for p in upd.split(',')] for upd in updates]
+        pages = parse([(int(x), int(y)) for x, y in [r.strip().split('|') for r in o.readlines()]])
+        page_numbers = [[int(p) for p in upd.split(',')] for upd in [u.strip() for u in u.readlines()]]
         solve_one(pages, page_numbers)
         solve_two(pages, page_numbers)
